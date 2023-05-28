3da490458f1b46932f53d28b4565aa2dda708f38
khudari.januune.xassan@gmail.com <khudari.januune.xassan@gmail.com>
2023-05-28 05:12:02 -1200

    cors solution

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ const mongoose = require('mongoose');
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
 const HttpError = require('./models/http-error');
+const cors =require('cors')
 
 const app = express();
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 app.use((req, res, next) => {
+
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Headers',
